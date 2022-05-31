fe23f20e3cfe2dd1799e30663680085807353e0a
Léo MOREAU <thecreepfire@gmail.com>
2022-05-31 10:59:09 +0200

    First Commit

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <p v-for="passenger in data" :key="passenger.id">{{ passenger.Name }}</p>
+  </div>
+</template>
+
+<script>
+/* import PassengerModel from '../models/passenger.js' */
+
+export default {
+  name: 'IndexPage',
+  data() {
+    return {
+      data: [],
+    }
+  },
+  created() {
+    this.getPassengers()
+  },
+  methods: {
+    /* Api Call on localhost:3000/data that puts into this.data tab */
+    async getPassengers() {
+      await this.$axios({
+        method: 'get',
+        url: '/api/data',
+      })
+        .then((response) => {
+          this.data = response.data
+        })
+        .catch((error) => {
+          // eslint-disable-next-line no-console
+          console.log(error)
+        })
+    },
+  },
+}
+</script>
